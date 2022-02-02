3d4485694d1a1a44fe155d663655cd8ca1db1303
saptarshi1996 <saptarshisg96@gmail.com>
2022-02-02 20:19:48 +0530

    add exceptions

--- anime.py
+++ anime.py
@@ -10,16 +10,17 @@ from scraper import (
 
 
 def get_episode_url(end_point: str) -> str:
-    return url_list["base_url"] + end_point
+    try:
+        return url_list["base_url"] + end_point
+    except KeyError as ke:
+        raise ke
 
 
 def search_anime(search_term: str) -> list:
     try:
-
         search_url = url_list["search_url"] + search_term
         anime_list = search_scraper(url=search_url)
         return anime_list
-
     except Exception as e:
         raise e
 
@@ -30,17 +31,14 @@ def search_anime_episode_list(anime: dict) -> list:
         episode_list = scrape_episode_list(anime_page_url)
         return episode_list
     except Exception as e:
-        print(e)
-        return []
+        raise e
 
 
 def get_anime_episode(episode: dict) -> list:
     try:
-
         episode_page_url = get_episode_url(episode["url"])
         episode_url, download_url = scrape_episode(episode_page_url)
         return get_episode_url(episode_url), get_episode_url(download_url)
-
     except Exception as e:
         print(e)
         return [None, None]
--- main.py
+++ main.py
@@ -19,28 +19,24 @@ def main() -> None:
 
             anime_list = search_anime(anime)
             for (i, anime) in enumerate(anime_list):
-                print(("{0}. {1}").format(i + 1, anime["title"]))
+                print(f"{i+1}. {anime['title']}")
             print("\n")
 
             anime_choice = int(input("Choose an anime by serial no.\n"))
             chosen_anime = anime_list[anime_choice - 1]
-            print(
-                "You chose {0}. Searching for episodes...".format(chosen_anime["title"])
-            )
+            print(f"You chose {chosen_anime['title']}. Searching for episodes...")
 
             episode_list = search_anime_episode_list(chosen_anime)
             for (i, episode) in enumerate(episode_list):
-                print(("{0}. {1}").format(i + 1, episode["title"]))
+                print(f"{i+1}. {episode['title']}")
 
             episode_choice = int(input("Choose an episode by serial no.\n"))
             chosen_episode = episode_list[episode_choice - 1]
-            print("You chose {0}. Searching...".format(chosen_episode["title"]))
+            print(f"You chose {chosen_episode['title']}. Searching...")
 
             episode_url, download_url = get_anime_episode(chosen_episode)
             print(
-                "To watch, ctrl+click on {0}. To download, ctrl+click on {1}".format(
-                    episode_url, download_url
-                )
+                f"To watch, ctrl+click on {episode_url}. To download, ctrl+click on {download_url}"
             )
     except KeyError as ke:
         raise ke
